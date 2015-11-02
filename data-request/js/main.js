9cb843a038f4117253fd0d02b60356f5c740e52a
Trent Rosane <trosane@uw.edu>
2015-11-02 14:14:49 -0800

    final

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,9 @@
 // Create application
-
+var app = angular.module('myApp', []);
+var ctrl = app.controller('myCtrl', function($scope, $http) {
+    $http.get('http://www.w3schools.com/angular/customers.php').then(function(response) {
+        $scope.names = response.data.records;
+    });
+});
 
 // Bind controller customersCtrl
